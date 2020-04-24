691a6a795d246039bf30436a000ca52e44e2d63b
marek sitarski <marek_sitarski@vp.pl>
2020-04-24 23:57:53 +0200

    Add basic location to feature

--- app/src/main/java/theNorthApplication/app/AppApplication.java
+++ app/src/main/java/theNorthApplication/app/AppApplication.java
@@ -1,19 +1,14 @@
 package theNorthApplication.app;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import theNorthApplication.app.parser.ShopsSearcherParser;
-import theNorthApplication.app.service.SearcherService;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
 
-	}
+    }
 
 }
