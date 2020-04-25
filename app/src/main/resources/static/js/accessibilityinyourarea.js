93ef8c9696be42fc29f4e24d23de40567bb7d910
marek sitarski <marek_sitarski@vp.pl>
2020-04-25 14:32:42 +0200

    Small fix - zoom

--- app/src/main/resources/static/js/accessibilityinyourarea.js
+++ app/src/main/resources/static/js/accessibilityinyourarea.js
@@ -1,14 +1,14 @@
 var map, infoWindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 52.257532, lng: 18.993839},
-        zoom: 6
+        center: {lat: 55.257532, lng: 18.993839},
+        zoom: 19
     });
     infoWindow = new google.maps.InfoWindow;
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
+            var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
@@ -19,7 +19,7 @@ function initMap() {
             map.setCenter(pos);
 
             var markerCoordinates = {lat: 54.489808, lng: 18.533537};
-            let marker = new google.maps.Marker({position: markerCoordinates, map: map});
+            var marker = new google.maps.Marker({position: markerCoordinates, map: map});
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
