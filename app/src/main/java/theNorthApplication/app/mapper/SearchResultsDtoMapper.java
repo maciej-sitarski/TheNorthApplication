4738a469fe57bc63da82f68332bd2a1d89ac748c
marek sitarski <marek_sitarski@vp.pl>
2020-04-25 14:04:47 +0200

    Add lat/lon to StoreDto and fix mapper

--- app/src/main/java/theNorthApplication/app/mapper/SearchResultsDtoMapper.java
+++ app/src/main/java/theNorthApplication/app/mapper/SearchResultsDtoMapper.java
@@ -13,15 +13,38 @@ public class SearchResultsDtoMapper {
 
         StoreDto storeDto = new StoreDto();
         String[] address = results.getAddress().split(",");
-        address[1] = address[1].substring(7);
+
+        String country = null;
+        String street = null;
+        String town = null;
+        if (address.length>2) {
+            country = address[2];
+            street = address[0];
+            town = getTownFormAddress(address[1]);
+        } else {
+            country = address[1];
+            street = "";
+            town = getTownFormAddress(address[0]);
+        }
+        String lat = results.getGeometry().getLocation().getLat();
+        String lon = results.getGeometry().getLocation().getLat();
 
         storeDto.setId(results.getId());
         storeDto.setName(results.getName());
-        storeDto.setCountry(address[2]);
-        storeDto.setStreet(address[0]);
-        storeDto.setTown(address[1]);
+        storeDto.setCountry(country);
+        storeDto.setStreet(street);
+        storeDto.setTown(town);
+        storeDto.setLat(lat);
+        storeDto.setLon(lon);
         return storeDto;
     }
 
-
+    private String getTownFormAddress(String postalCodeAndTown) {
+        String[] separatedPostalCodeAndTown = postalCodeAndTown.stripLeading().split(" ");
+        if (separatedPostalCodeAndTown.length>1) {
+            return separatedPostalCodeAndTown[1];
+        } else {
+            return separatedPostalCodeAndTown[0];
+        }
+    }
 }
