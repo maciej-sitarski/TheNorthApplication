2344b6efcb4e793d98e338ad76cae7024a549994
Mikolaj-Kr <mikolak25@gmail.com>
2020-04-26 15:13:36 +0200

    Ready

--- app/src/main/java/theNorthApplication/app/dto/ShopNameDto.java
+++ app/src/main/java/theNorthApplication/app/dto/ShopNameDto.java
@@ -2,7 +2,10 @@ package theNorthApplication.app.dto;
 
 import lombok.Data;
 
+import java.util.StringTokenizer;
+
 @Data
 public class ShopNameDto {
     String name;
+    String type;
 }
--- app/src/main/java/theNorthApplication/app/parser/ShopsSearcherParser.java
+++ app/src/main/java/theNorthApplication/app/parser/ShopsSearcherParser.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Service;
 import theNorthApplication.app.api.SearchResults;
 import org.springframework.web.context.WebApplicationContext;
 import theNorthApplication.app.api.searcherClasses.Results;
+import theNorthApplication.app.dto.ShopNameDto;
+import theNorthApplication.app.service.ShopsNamesService;
 
-import javax.xml.transform.Result;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,8 +24,13 @@ import java.util.List;
 public class ShopsSearcherParser {
 
     private ObjectMapper objectMapper = new ObjectMapper();
+    private final ShopsNamesService shopsNamesService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     String apiKey = "AIzaSyByf6Wfz_btg3iIOcdwav_UCOJGucPln4g";
+
+    public ShopsSearcherParser(ShopsNamesService shopsNamesService) {
+        this.shopsNamesService = shopsNamesService;
+    }
 
     public SearchResults parseSearch(String shop, String town) throws UnirestException, IOException, InterruptedException {
 
@@ -58,18 +65,18 @@ public class ShopsSearcherParser {
         return searchResults;
     }
 
-    public SearchResults parseSearchByCoordinatesAndRadiusByM(String lat, String lng, String radius) throws IOException, UnirestException, InterruptedException {
-        SearchResults searchResults = getResponseFromApiByCoordinatesAndRadiusByM(lat, lng, radius);
-        logger.info(lat + " and " + lng + " and " + radius);
+    public SearchResults parseSearchByCoordinatesAndRadiusByM(String lat, String lng, String radius) throws IOException, UnirestException{
+        List<ShopNameDto> shopsNames = shopsNamesService.getShopsNames();
+        SearchResults searchResults = getResponseFromApiByCoordinatesAndRadiusByM(lat, lng, radius, shopsNames.get(0).getName(), shopsNames.get(0).getType());
+        shopsNames.remove(0);
+        shopsNames.forEach(shopNameDto -> {
+            try {
+                searchResults.getResultsList().addAll(getResponseFromApiByCoordinatesAndRadiusByM(lat, lng, radius, shopNameDto.getName(), shopNameDto.getType()).getResultsList());
+            } catch (UnirestException | IOException e) {
+                e.printStackTrace();
+            }
+        });
 
-        SearchResults nexPageResult;
-
-        while (searchResults.getNextPageToken() != null) {
-            Thread.sleep(1600L);
-            nexPageResult = getNextPageResults(searchResults.getNextPageToken());
-            nexPageResult.getResultsList().forEach(results -> searchResults.getResultsList().add(results));
-            searchResults.setNextPageToken(nexPageResult.getNextPageToken());
-        }
         return searchResults;
     }
 
@@ -98,9 +105,9 @@ public class ShopsSearcherParser {
         return objectMapper.readValue(response.getBody(), SearchResults.class);
     }
 
-    private SearchResults getResponseFromApiByCoordinatesAndRadiusByM(String lat, String lng, String radius) throws UnirestException, IOException {
+    private SearchResults getResponseFromApiByCoordinatesAndRadiusByM(String lat, String lng, String radius, String shopName, String shopType) throws UnirestException, IOException {
 
-        String requestUri = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ lat + "," + lng +"&radius="+ radius + "&type=grocery_or_supermarket&key=" + apiKey);
+        String requestUri = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ lat + "," + lng +"&radius="+ radius + "&type="+ shopType + "&name=" + shopName + "&key=" + apiKey);
 
         HttpResponse<String> response = Unirest.get(requestUri).asString();
         return objectMapper.readValue(response.getBody(), SearchResults.class);
