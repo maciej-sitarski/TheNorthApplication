649046e4e893f8533da8811c4c8bbd6d78f32707
marek sitarski <marek_sitarski@vp.pl>
2020-04-26 15:46:11 +0200

    new approach testing

--- app/src/main/java/theNorthApplication/app/restcontroller/NearbyShopsRestController.java
+++ app/src/main/java/theNorthApplication/app/restcontroller/NearbyShopsRestController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import theNorthApplication.app.dto.StoreDto;
+import theNorthApplication.app.dto.StoreEntityDto;
 import theNorthApplication.app.service.SearcherService;
+import theNorthApplication.app.service.StoreService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class NearbyShopsRestController {
@@ -17,10 +20,24 @@ public class NearbyShopsRestController {
     @Autowired
     private SearcherService searcherService;
 
+    @Autowired
+    private StoreService storeService;
+
     @GetMapping("/rest/api/nearbyshops")
     public List<StoreDto> getNearbyShops(@RequestParam("lat") String lat,
                                          @RequestParam("lng") String lng,
                                          @RequestParam("radius") String radius) throws IOException, UnirestException, InterruptedException {
-        return searcherService.getStoresByCoordinatesAndRadius(lat, lng, radius);
+
+        List<StoreDto> storeDtos = searcherService.getStoresByCoordinatesAndRadius(lat, lng, radius);
+
+        List<StoreDto> newStoreDtos = storeDtos.stream().map(store -> {
+            String storeId = store.getId();
+            StoreEntityDto storeEntityDto1 = storeService.findStoreById(storeId);
+            store.setStoreEntityDto(storeEntityDto1);
+            return store;
+            }
+        ).collect(Collectors.toList());
+
+        return newStoreDtos;
     }
 }
