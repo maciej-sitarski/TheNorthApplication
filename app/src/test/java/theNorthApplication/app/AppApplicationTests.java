32b34285cb7b52fad05507777a7b7774075400d4
Mikolaj-Kr <mikolak25@gmail.com>
2020-05-04 12:11:26 +0200

    next try

--- app/src/test/java/theNorthApplication/app/AppApplicationTests.java
+++ app/src/test/java/theNorthApplication/app/AppApplicationTests.java
@@ -1,13 +1,13 @@
-//package theNorthApplication.app;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AppApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package theNorthApplication.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
